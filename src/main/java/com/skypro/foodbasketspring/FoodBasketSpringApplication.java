9edad2af90b252f0143dec74fd8ef78258062d61
ruslaan.gilazefwgm <ruslaan.gilazefwgm@gmail.com>
2022-11-16 10:48:56 +0300

    HW

---
+++ src/main/java/com/skypro/foodbasketspring/FoodBasketSpringApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.foodbasketspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodBasketSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodBasketSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skypro/foodbasketspring/FoodBasketSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.foodbasketspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodBasketSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
